9c21e6753601b1a343ab9abf7152b11059dc122b
Is <isaiaslflores@gmail.com>
2025-10-22 19:24:29 -0500

    cors render ok

--- server/src/cors.config.ts
+++ server/src/cors.config.ts
@@ -13,7 +13,6 @@ export const corsConfig = (whitelist: string[] = ACCEPTED_ORIGINS) => {
       origin: string | undefined,
       callback: (err: Error | null, origin?: StaticOrigin) => void,
     ) {
-      console.log(origin);
       if (!origin || whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
